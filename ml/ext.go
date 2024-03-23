64ef3f0497f36d65281b23d7f1521af047f27f4f
alan890104 <alan890104@gmail.com>
2024-03-23 18:13:31 +0800

    Refactor ModelLoader interface and its implementations

--- ml/ext.go
+++ ml/ext.go
@@ -16,10 +16,10 @@ const (
 )
 
 type ModelLoader interface {
-	LoadGeMM(ctx *Context, input_size uint32, output_size uint32) Layer
-	LoadActivation(ctx *Context, act ACT) Layer
-	LoadConv1D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer
-	LoadConv2D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer
+	GeMM(ctx *Context, input_size uint32, output_size uint32) Layer
+	Activation(ctx *Context, act ACT) Layer
+	Conv1D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer
+	Conv2D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer
 }
 
 type FileModelLoaderImpl struct {
@@ -30,8 +30,8 @@ func NewFileModelLoaderImpl(fp *os.File) ModelLoader {
 	return &FileModelLoaderImpl{fp: fp}
 }
 
-// LoadActivation implements ModelLoader.
-func (f *FileModelLoaderImpl) LoadActivation(ctx *Context, act ACT) Layer {
+// Activation implements ModelLoader.
+func (f *FileModelLoaderImpl) Activation(ctx *Context, act ACT) Layer {
 	switch act {
 	case ACT_RELU:
 		return func(input *Tensor) *Tensor {
@@ -46,18 +46,18 @@ func (f *FileModelLoaderImpl) LoadActivation(ctx *Context, act ACT) Layer {
 	}
 }
 
-// LoadConv1D implements ModelLoader.
-func (f *FileModelLoaderImpl) LoadConv1D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer {
+// Conv1D implements ModelLoader.
+func (f *FileModelLoaderImpl) Conv1D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer {
 	panic("unimplemented")
 }
 
-// LoadConv2D implements ModelLoader.
-func (f *FileModelLoaderImpl) LoadConv2D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer {
+// Conv2D implements ModelLoader.
+func (f *FileModelLoaderImpl) Conv2D(ctx *Context, input_size uint32, output_size uint32, kernel_size uint32, stride uint32, padding uint32) Layer {
 	panic("unimplemented")
 }
 
-// LoadGeMM implements ModelLoader.
-func (f *FileModelLoaderImpl) LoadGeMM(ctx *Context, input_size uint32, output_size uint32) Layer {
+// GeMM implements ModelLoader.
+func (f *FileModelLoaderImpl) GeMM(ctx *Context, input_size uint32, output_size uint32) Layer {
 	w := NewTensor2D(nil, TYPE_F32, input_size, output_size)
 	for i := 0; i < len(w.Data); i++ {
 		w.Data[i] = readFP32(f.fp)
